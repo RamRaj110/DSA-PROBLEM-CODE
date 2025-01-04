f5771e01ef8393c65fea09b5aabc5346e765c83b
RamRaj110 <rampawankumar110@gmail.com>
2025-01-04 20:15:17 +0530

    DSA Problem Code

---
+++ String/1047LeetCode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        //Take Empty String.
+       string ans = "";
+
+       int i = 0;
+       while(i < s.length()){
+            if(ans.length() > 0 && ans[ans.length() -1] == s[i]){
+                //Remove Last Char.whose in empty string.
+                ans.pop_back();
+            }else{
+                //Add Char whose not in empty string.
+                ans.push_back(s[i]);
+            }
+            i++;
+       }
+       return ans;
+    }
+};
